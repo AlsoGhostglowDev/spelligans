8542f75168de9d0dd1405631baaad56556ffc867
TBar09 <131258758+TBar09@users.noreply.github.com>
2024-06-14 14:58:11 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -3,6 +3,16 @@ package;
 import flixel.FlxGame;
 import openfl.display.Sprite;
 
+//Thanks Sqirra-rng
+#if SHOW_CRASHDUMP
+import openfl.events.UncaughtErrorEvent;
+import haxe.CallStack;
+import haxe.io.Path;
+import sys.FileSystem;
+import sys.io.File;
+import sys.io.Process;
+import lime.app.Application;
+#end
 class Main extends Sprite
 {
 	public function new()
@@ -10,5 +20,34 @@ class Main extends Sprite
 		super();
 		GameState.skipTrans = true;
 		addChild(new FlxGame(0, 0, states.MainMenuState));
+		#if SHOW_CRASHDUMP
+		openfl.Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onCrash);
+		#end
 	}
+
+	#if SHOW_CRASHDUMP
+	function onCrash(e:UncaughtErrorEvent):Void
+	{
+		var errMsg:String = "";
+		var path:String;
+		var callStack:Array<StackItem> = CallStack.exceptionStack(true);
+		for (stackItem in callStack)
+		{
+			switch (stackItem)
+			{
+				case FilePos(s, file, line, column):
+					errMsg += file + " (line " + line + ")\n";
+				default:
+					Sys.println(stackItem);
+			}
+		}
+
+		errMsg += "\nUncaught Error: " + e.error;
+
+		Sys.println(errMsg);
+
+		Application.current.window.alert(errMsg, "Erm.");
+		Sys.exit(1);
+	}
+	#end
 }
